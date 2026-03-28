facad2262dc338637ec065d56377092bf8fc4315
AYUSMAN SAMASI <hariswarsamasi@gmail.com>
2026-03-28 11:08:46 +0530

    Update checkinRoutes.js

--- routes/checkinRoutes.js
+++ routes/checkinRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/checkinController");
+const multer = require("multer");
 
-router.post("/create", controller.createCheckin);
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+router.post("/create", upload.single("pic"), controller.createCheckin);
 router.get("/all", controller.getCheckins);
 
 module.exports = router;
